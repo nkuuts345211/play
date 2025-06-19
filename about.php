ef6ce214d8928174466f8ed47655b6b31fe5a20b
nkuuts345211 <aistu112534@gmail.com>
2025-06-19 13:41:02 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>關於</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="top" align="center">網頁製作者</div>    
+    <nav class="link" align="center"><a href="/">首頁</a>|<a href="about.php">關於</a></nav>
+    <div class="main" align="center">製作人<br><h2>鄧益汯</h2></div>
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自我學習歷程首頁</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="top" align="center">學習歷程</div>
+    <nav class="link" align="center"><a href="/">首頁</a>|<a href="about.php">關於</a></nav>
+    <div class="main"></div>
+</body>
+</html>
